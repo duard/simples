f4dac79ede55034abf421452d59e94aa6af716b3
Carlos Aquino <duardbr@gmail.com>
2020-12-04 10:32:46 -0300

    Diretivas funcionando

--- libs/pages/src/lib/test/test.module.ts
+++ libs/pages/src/lib/test/test.module.ts
@@ -1,18 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AppDirectivesModule } from '@simples/app-directives';
 import { AppSharedModule } from '@simples/app-shared';
+import { AppTestsModule } from '@simples/app-tests';
 
 import { TestRoutingModule } from './test-routing.module';
 import { TestComponent } from './test.component';
 
 @NgModule({
   declarations: [TestComponent],
-  imports: [
-    CommonModule,
-    TestRoutingModule,
-    AppDirectivesModule,
-    AppSharedModule,
-  ],
+  imports: [CommonModule, AppSharedModule, TestRoutingModule, AppTestsModule],
 })
 export class TestModule {}
