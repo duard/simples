e374825f426e38a934f76684bd7be5950bb6401c
Carlos Aquino <duardbr@gmail.com>
2020-12-04 10:28:30 -0300

    Criando o loading global

--- libs/pages/src/lib/test/test.module.ts
+++ libs/pages/src/lib/test/test.module.ts
@@ -1,12 +1,18 @@
-import { AppTestsModule } from '@simples/app-tests';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AppDirectivesModule } from '@simples/app-directives';
+import { AppSharedModule } from '@simples/app-shared';
 
 import { TestRoutingModule } from './test-routing.module';
 import { TestComponent } from './test.component';
 
 @NgModule({
   declarations: [TestComponent],
-  imports: [CommonModule, TestRoutingModule, AppTestsModule],
+  imports: [
+    CommonModule,
+    TestRoutingModule,
+    AppDirectivesModule,
+    AppSharedModule,
+  ],
 })
 export class TestModule {}
